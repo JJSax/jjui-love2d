cd794fb9186fd9ac51fdb19651af9f06f143ef01
JJSax <5084938+JJSax@users.noreply.github.com>
2021-11-09 21:27:18 -0700

    add methods to init to match updated libraries.

--- ui/init.lua
+++ ui/init.lua
@@ -1,10 +1,10 @@
-local ui = {_version = "0.0.33"}
+local ui = {_version = "0.0.4"}
 local HERE = ...
 local MODULES = HERE..".modules"
 
 ui.modules = {}
 ui.modules.button = require(MODULES..".button")
-ui.modules.dial, ui.modules.arc = require(MODULES..".radialbuttons")()
+ui.modules.dial = require(MODULES..".dial")
 ui.modules.scroll = require(MODULES..".scrolllib")
 ui.modules.slider = require(MODULES..".slider")
 ui.modules.set = require(MODULES..".uisets")
@@ -12,8 +12,10 @@ ui.modules.set = require(MODULES..".uisets")
 ui.newRectangleButton = ui.modules.button.newRectangleButton
 ui.newPolygonButton = ui.modules.button.newPolygonButton
 ui.newCircleButton = ui.modules.button.newCircleButton
+ui.newArcButton = ui.modules.button.newArcButton
+ui.newAngleButton = ui.modules.button.newAngleButton
+
 ui.newDial = ui.modules.dial.new
-ui.newArcButton = ui.modules.arc.new
 ui.newScrollViewport = ui.modules.scroll.newViewport
 ui.newSlider = ui.modules.slider.new
 ui.newSet = ui.modules.set.new
