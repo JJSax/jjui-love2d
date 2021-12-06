e2a68a794bd24b65d730b3d0ac7378c3ffa92938
JJSax <5084938+JJSax@users.noreply.github.com>
2021-12-05 21:54:18 -0700

    [button] Unify set functions; Fix prompt; Add Held
    Possibly final beta update.  I'm fairly happy with this code.  This removes redundant set functions and puts them into "setVar".  Prompt has been fixed and is working; default clamps inside window.  
    Add held callbacks, for starting to hold, holding and stop holding.

--- ui/modules/button.lua
+++ ui/modules/button.lua
@@ -1,11 +1,7 @@
 
---[[
-]]
-
-
 local button = {}
 button.__index = button
-button._version = "0.8.0"
+button._version = "0.9.0"
 
 local ORIGIN = {x = 0, y = 0}
 
@@ -19,10 +15,10 @@ local lg = love.graphics
 local lg = love.graphics
 
 local function dist(x1, y1, x2, y2, squared)
-  local dx = x1 - x2
-  local dy = y1 - y2
-  local s = dx * dx + dy * dy
-  return squared and s or math.sqrt(s)
+	local dx = x1 - x2
+	local dy = y1 - y2
+	local s = dx * dx + dy * dy
+	return squared and s or math.sqrt(s)
 end
 
 local function angle(x1, y1, x2, y2)
@@ -31,6 +27,12 @@ end
 
 local function vector(angle, magnitude)
 	return math.cos(angle) * magnitude, math.sin(angle) * magnitude
+end
+
+local function clamp(num, low, high)
+	if num < low then return low end
+	if num > high then return high end
+	return num
 end
 
 -- return image from string path or passed drawable
@@ -105,21 +107,21 @@ local function getDefault()
 		fitText = false, fitHoverText = true, visible = true,
 		requireSelfClick = true, -- require press to happen in this button, then release in this button.
 		parent = ORIGIN,
+		triggerMouse = {1}, triggerKeyboard = {}, -- array of buttons to detect isDown
 
 		-- Keep in mind, you cannot be pressed while not being hovered.
 		-- Variables for when the button is not hovered, or selected.
 		text = "", -- text to print
 		textRotation = 0,
-		textColor = {1,1,1,1}, -- color of text
-		textBackgroundColor = {0,0,0,0}, -- DOUBLE CHECK highlight color of text?
+		textColor = {1,1,1,1}, -- Color of text
+		textBackgroundColor = {0,0,0,0}, -- Color of box behind text
 		textBackgroundBuffer = 1, -- Buffer in pixels for surrounding highlight.
 		textXOffset = 0, -- Horizontal text offset within button.  Note, doesn't have to be inside button.
 		textYOffset = 0, -- Vertical offset within button
 		image = nil, -- Image to draw, has to be lg.newImage, not path to image.  You can use formatImage.
-		color = {1,1,1,1}, -- color of main button area.
+		color = {1,1,1,1}, -- Color of main button area.
 		outlineColor = {0,0,0,0}, -- Color of outline draw.
 		outlineWidth = 0, -- Width of outline
-		-- isHovering
 
 		-- Button state while hovered, but not selected.
 		hoverText = "",
@@ -132,13 +134,9 @@ local function getDefault()
 		hoverOutlineColor = {0,0,0,0},
 		hoverOutlineWidth = 1,
 		isHovering = false,
-		 -- additional hover variables
-		promptOffsetX = 0, promptOffsetY = 0,
+		 -- additional hover variable
 		hoverTime = 0,
 		hoverFuncTime = 1,
-		hoverPromptTime = 1,
-		isPrompting = false,
-		lockPromptToWindow = true,
 
 		-- Button state while pressed but not selected,
 		 -- AKA trigger mouse/keyboard held while hovering over the button not yet triggered.
@@ -164,8 +162,6 @@ local function getDefault()
 		selectedOutlineColor = {0,0,0,0},
 		selectedOutlineWidth = 1,
 		selected = false,
-		selectable = true,
-		-- selectable refers to if it can be selected despite if it is visible.  Usually will == visible
 
 		-- Button state selected and hovered, not pressed
 		hoverSelectedText = "",
@@ -189,20 +185,23 @@ local function getDefault()
 		pressedSelectedOutlineColor = {0,0,0,0},
 		pressedSelectedOutlineWidth = 1,
 
-		--
-		hoverPromptText = "",
-		hoverPromptTextRotation = 0,
+		-- Prompting
+		promptText = "",
 		promptFont = lg.getFont(),
 		promptTextColor = {1,1,1,1},
-		promptTextBackgroundColor = {0,0,0,0},
-		promptTextBackgroundBuffer = 1,
 		promptColor = {0,0,0,0},
 		promptOutlineColor = {0,0,0,0},
 		promptOutlineWidth = 1,
-		promptXBuffer = 2, -- this is the extra space around prompt text
-		promptYBuffer = 2, -- this is the extra space around prompt text
+		promptGap = {2, 2}, -- this is the extra space around prompt text
+		promptOffset = {0, 0},
+		promptPosition = nil,
+		hoverPromptTime = 1,
+		isPrompting = false,
+		lockPromptToWindow = true,
 
-		triggerMouse = {1}, triggerKeyboard = {}, -- array of buttons to detect isDown
+		pressTime = 0,
+		heldTriggerTime = 1,
+		held = false,
 	}
 end
 
@@ -251,7 +250,7 @@ function button.newRectangleButton(l, t, w, h, extra)
 	)
 end
 
-function button.newArcButton(x, y, radius, angle1, angle2, options)
+function button.newArcButton(x, y, radius, angle1, angle2, extra)
 	local properties = getDefault()
 	properties.centerx, properties.centery = 0, 0
 	properties.x, properties.y, properties.radius = x, y, radius
@@ -259,7 +258,7 @@ function button.newArcButton(x, y, radius, angle1, angle2, options)
 	properties.shape = lg.arc
 	properties.arctype = "pie"
 	properties.textOrientation = "angled"
-	properties = merge(properties, options)
+	properties = merge(properties, extra)
 
 	return setmetatable(properties, button)
 end
@@ -290,6 +289,21 @@ function button:update(dt)
 			self.hover()
 		end
 		self.hoverTime = self.hoverTime + dt
+
+		if self.isPressed then
+			self.pressTime = self.pressTime + dt
+			if self.pressTime > self.heldTriggerTime then
+				if not self.held then
+					self.held = true
+					self:onholdStart()
+				end
+				self:onHold()
+			end
+		else
+			self.held = false
+			self:onHoldStop()
+		end
+
 		self.isPrompting = self.hoverTime > self.hoverPromptTime
 	elseif self.isHovering then
 		self.onExit()
@@ -297,71 +311,73 @@ function button:update(dt)
 		self.hoverTime = 0
 		self.isPressed = false
 		self.isPrompting = false
+		self.held = false
+		self:onHoldStop()
 	end
 end
 
 function button:draw()
+	if not self.visible then return false end
 	lg.push()
 	local fV = formatVariable
+	local v = {}
+	if self.isHovering then table.insert(v, "hover") end
+	if self.isPressed then table.insert(v, "pressed") end
+	if self.selected then table.insert(v, "selected") end
 
-	if self.visible then
-		local v = {}
-		if self.isHovering then table.insert(v, "hover") end
-		if self.isPressed then table.insert(v, "pressed") end
-		if self.selected then table.insert(v, "selected") end
+	local x, y = self.x + self.parent.x, self.y + self.parent.y
+	lg.translate(x, y)
 
-		local x, y = self.x + self.parent.x, self.y + self.parent.y
-		lg.translate(x, y)
+	-- draw
+	lg.setColor(self[fV("color", v)])
+	local im = self[fV("image", v)]
+	if im then
+		local w, h = self.w or self.radius * 2, self.h or self.radius * 2
+		lg.draw(im, 0, 0, self.rotation,
+			w/im:getWidth(), h/im:getHeight(),
+			self.radius and im:getWidth()/2, self.radius and im:getHeight()/2
+		)
+	else
+		local shapePack = {
+			[lg.polygon] = {self.vertices},
+			[lg.circle] = {0,0, self.radius},
+			[lg.arc] = {0, 0, self.radius, self.angle1, self.angle2}
+		}
+		self.shape("fill", unpack(shapePack[self.shape]))
+		lg.setColor(self[fV("outlineColor", v)])
+		lg.setLineWidth(self[fV("outlineWidth", v)])
+		self.shape("line", unpack(shapePack[self.shape]))
+	end
 
-		-- draw
-		lg.setColor(self[fV("color", v)])
-		local im = self[fV("image", v)]
-		if im then
-			local w, h = self.w or self.radius * 2, self.h or self.radius * 2
-			lg.draw(im, 0, 0, self.rotation,
-				w/im:getWidth(), h/im:getHeight(),
-				self.radius and im:getWidth()/2, self.radius and im:getHeight()/2
-			)
-		else
-			local shapePack = {
-				[lg.polygon] = {self.vertices},
-				[lg.circle] = {0,0, self.radius},
-				[lg.arc] = {0, 0, self.radius, self.angle1, self.angle2}
-			}
-			self.shape("fill", unpack(shapePack[self.shape]))
-			lg.setColor(self[fV("outlineColor", v)])
-			lg.setLineWidth(self[fV("outlineWidth", v)])
-			self.shape("line", unpack(shapePack[self.shape]))
-		end
+	-- rectangle for text background
+	lg.setColor( self[fV("textBackgroundColor", v)] )
+	local txt =  self[fV("text", v)]
+	local tbb =  self[fV("textBackgroundBuffer", v)]
+	local tr  =  self[fV("textRotation", v)]
 
-		-- rectangle for text background
-		lg.setColor( self[fV("textBackgroundColor", v)] )
-		local txt =  self[fV("text", v)]
-		local tbb =  self[fV("textBackgroundBuffer", v)]
-		local tr  =  self[fV("textRotation", v)]
-
-		lg.translate(self.textXOffset, self.textYOffset)
-		local tw, th = self.font:getWidth(txt), self.font:getHeight(txt)
-		local txtx, txty = self.centerx + self.textXOffset,
-			self.centery + self.textYOffset
-		if self.shape == lg.arc then
-			if self.textOrientation == "angled" then
-				tr = self:getCenterAngle()
-				txtx, txty = vector(tr, self.radius/1.5)
-				local halfpi = math.pi/2
-				if tr > halfpi and tr <= halfpi*3 then
-					tr = tr + -math.pi
-				end
+	lg.translate(self.textXOffset, self.textYOffset)
+	local tw, th = self.font:getWidth(txt), self.font:getHeight(txt)
+	local txtx, txty = self.centerx + self.textXOffset,
+		self.centery + self.textYOffset
+	if self.shape == lg.arc then
+		if self.textOrientation == "angled" then
+			tr = self:getCenterAngle()
+			txtx, txty = vector(tr, self.radius/1.5)
+			local halfpi = math.pi/2
+			if tr > halfpi and tr <= halfpi*3 then
+				tr = tr + -math.pi
 			end
 		end
-		lg.rectangle("fill",
-			txtx - tbb - tw/2,	txty - tbb - th/2,
-			tw + tbb*2, th + tbb*2
-		)
-		lg.setColor(self[fV("textColor", v)])
-		lg.setFont(self.font)
-		lg.print(txt, txtx, txty, tr, 1, 1, self.font:getWidth(txt)/2, self.font:getHeight()/2)
 	end
+	lg.rectangle("fill",
+		txtx - tbb - tw/2,	txty - tbb - th/2,
+		tw + tbb*2, th + tbb*2
+	)
+	lg.setColor(self[fV("textColor", v)])
+	lg.setFont(self.font)
+	lg.print(txt, txtx, txty, tr, 1, 1,
+		math.floor(self.font:getWidth(txt)/2), math.floor(self.font:getHeight()/2))
+
 
 	lg.pop()
 end
@@ -394,18 +410,19 @@ function button:mouseIsDown()
 end
 
 function button:keypressed(key, istouch, presses)
-	if self:inBounds(love.mouse.getPosition()) then
-		if inside(self.triggerKeyboard, key) then
-			self.origPress = true
-			self:onPress(key, istouch, presses)
-		end
+	if not self:inBounds(love.mouse.getPosition()) then return end
+	if inside(self.triggerKeyboard, key) then
+		self.origPress = true
+		local x, y = love.mouse.getPosition()
+		self:onPress(x, y, key, istouch, presses)
 	end
 end
 function button:keyreleased(key, istouch, presses)
 	if self:inBounds(love.mouse.getPosition()) then
 		if self.requireSelfClick and self.origPress or not self.requireSelfClick then
 			if inside(self.triggerKeyboard, key) then
-				self:onRelease(key, istouch, presses)
+				local x, y = love.mouse.getPosition()
+				self:onRelease(x, y, key, istouch, presses)
 			end
 		end
 	end
@@ -423,26 +440,34 @@ end
 -- draws popup message  Put in love's draw loop after other buttons it may overlap
 function button:prompt()
 	if not self.isPrompting then return false end
-	lg.push()
 
+	local buffX, buffY = unpack(self.promptGap)
+	local font = self.promptFont
+	local text = self.promptText
 	local mx, my = love.mouse.getPosition()
-	local pW, pY = self.font:getWidth(self.hoverPromptText) + self.promptXBuffer * 2,
-		self.font:getHeight(self.hoverPromptText) + self.promptYBuffer * 2
+	mx, my = mx + self.promptOffset[1], my + self.promptOffset[2]
+	local pW, pY = font:getWidth (text) + buffX * 2,
+				   font:getHeight(text) + buffY * 2
 
-	-- these two lines slightly buggy.  fix it
 	if self.lockPromptToWindow then
-		if mx + pW > lg.getWidth() then mx = lg.getWidth() - pW end
-		if my - pY/2 < 0 then my = pY/2 end
+		mx = clamp(mx, 0, lg.getWidth() - font:getWidth(text))
+		my = clamp(my, 0, lg.getHeight() - font:getHeight())
 	end
+	local rectX, rectY = mx - buffX, my - buffY
+
+	if self.promptPosition then
+		rectX, rectY = unpack(self.promptPosition)
+	end
+
 	lg.setColor(self.promptColor)
-	lg.rectangle("fill", mx, my-self.font:getHeight(self.hoverPromptText), pW,pY)
+	lg.rectangle("fill", rectX, rectY, pW, pY)
 	lg.setColor(self.promptOutlineColor)
 	lg.setLineWidth(self.promptOutlineWidth)
-	lg.rectangle("line", mx, my-self.font:getHeight(self.hoverPromptText), pW, pY)
+	lg.rectangle("line", rectX, rectY, pW, pY)
 	lg.setColor(self.promptTextColor)
-	lg.print(self.hoverPromptText, mx + self.promptXBuffer, my- self.font:getHeight()/2)
-
-	lg.pop()
+	lg.setFont(font)
+	lg.print(text, rectX + buffX, rectY + buffY)
+	return true
 end
 
 
@@ -451,10 +476,12 @@ end
 --------------------------------------------
 function button:onRelease() end
 function button:onPress() end
-function button:onHoldPress() end
 function button:hover() end
 function button:onEnter() end
 function button:onExit() end
+function button:onholdStart() end
+function button:onHold() end
+function button:onHoldStop() end
 
 -- Returns if x,y position is inside boundary of button
 -- Adapted Polygon Collision from to Stack Overflow user Peter Gilmour
@@ -503,7 +530,8 @@ function button:anyIsDown(key)
 end
 
 -- Toggles if the button is considered selected or not.
--- If you want the button to have a toggled on/off state, run this inside it's onPress or onRelease functions
+-- If you want the button to have a toggled on/off state,
+	-- run this inside it's onPress or onRelease functions
 function button:toggle(bool)
 	self.selected = bool and bool or not self.selected
 end
@@ -516,6 +544,7 @@ end
 ---------------------
 ----Set Functions----
 ---------------------
+
 
 -- main set functions
 
@@ -534,144 +563,58 @@ end
 	true will set all button states to that variable
 	table of variable strings to set those variables
 		-- i.e. button:setText example
-		button:setText("this is a test button!", {"selectedText", "pressedSelectedText", "hoverSelectedText"})
+		button:setText("this is a test button!",
+			{"selectedText", "pressedSelectedText", "hoverSelectedText"})
 ]]
+function button:setVar(var, value, ...)
+
+	--[[
+		Valid Var is case sensitive.
+		This function is for a quick way to set multiple variables.
+	]]
+
+	local validVar = {
+		"Text", "TextRotation", "TextColor", "TextBackgroundColor",
+		"TextBackgroundBuffer", "Image", "Color", "OutlineColor",
+		"OutlineWidth"
+	}
+	assert(inside(validVar, var),
+		"Valid param1 not passed. valid options are\n"..
+		table.concat( validVar, "\n")
+	)
+
+	local input = ...
+	if input == true then
+		for v in simVarIter(var) do
+			self[v] = value
+		end
+	elseif not input then
+		self[var] = value
+	elseif type(input) == "table" then
+		for k,v in pairs( input ) do
+			self[v] = value
+		end
+	end
+end
+
 function button:setText(text, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("Text") do
-			self[v] = text
-		end
-	elseif not input then
-		self.text = text
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = text
-		end
-	end
-end
-
+	self:setVar("Text", text, ...) end
 function button:setTextRotation(rotation, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("TextRotation") do
-			self[v] = rotation
-		end
-	elseif not input then
-		self.textRotation = rotation
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = rotation
-		end
-	end
-end
-
+	self:setVar("TextRotation", rotation, ...) end
 function button:setTextColor(col, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("TextColor") do
-			self[v] = col
-		end
-	elseif not input then
-		self.textColor = col
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = col
-		end
-	end
-end
-
+	self:setVar("TextColor", col, ...) end
 function button:setTextBackgroundColor(col, ...)
-
-	local input = ...
-	if input == true then
-		for v in simVarIter("TextBackgroundColor") do
-			self[v] = col
-		end
-	elseif not input then
-		self.textBackgroundColor = col
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = col
-		end
-	end
-end
-
+	self:setVar("TextBackgroundColor", col, ...) end
 function button:setTextBackgroundBuffer(buffer, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("TextBackgroundBuffer") do
-			self[v] = buffer
-		end
-	elseif not input then
-		self.textBackgroundBuffer = buffer
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = buffer
-		end
-	end
-end
-
+	self:setVar("TextBackgroundBuffer", buffer, ...) end
 function button:setImage(image, ...)
-	local input = ...
-	if input == true then
-		local im = formatImage(image)
-		for v in simVarIter("Image") do
-			self[v] = im
-		end
-	elseif not input then
-		self.image = formatImage( image )
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = formatImage( image )
-		end
-	end
-end
-
+	self:setVar("Image", formatImage(image), ...) end
 function button:setColor(col, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("Color") do
-			self[v] = col
-		end
-	elseif not input then
-		self.color = col
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = col
-		end
-	end
-end
-
+	self:setVar("Color", col, ...) end
 function button:setOutlineColor(col, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("OutlineColor") do
-			self[v] = col
-		end
-	elseif not input then
-		self.outlineColor = col
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = col
-		end
-	end
-end
-
+	self:setVar("OutlineColor", col, ...) end
 function button:setOutlineWidth(w, ...)
-	local input = ...
-	if input == true then
-		for v in simVarIter("OutlineWidth") do
-			self[v] = w
-		end
-	elseif not input then
-		self.outlineWidth = w
-	elseif type(input) == "table" then
-		for k,v in pairs( input ) do
-			self[v] = w
-		end
-	end
-end
--- function to clone button
+	self:setVar("OutlineWidth", w, ...) end
+
 
 return button
