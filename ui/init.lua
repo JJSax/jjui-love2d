733aa3626c6800d5380733210acf6f1df3c9138e
JJSax <5084938+JJSax@users.noreply.github.com>
2022-10-21 14:01:05 -0600

    Add textbox creation to init

--- ui/init.lua
+++ ui/init.lua
@@ -1,4 +1,4 @@
-local ui = {_version = "0.0.4"}
+local ui = {_version = "0.0.5"}
 local HERE = ...
 local MODULES = HERE..".modules"
 
@@ -8,6 +8,7 @@ ui.modules.dial = require(MODULES..".dial")
 ui.modules.scroll = require(MODULES..".scrolllib")
 ui.modules.slider = require(MODULES..".slider")
 ui.modules.set = require(MODULES..".uisets")
+ui.modules.textbox = require(MODULES..".textbox")
 
 ui.newRectangleButton = ui.modules.button.newRectangleButton
 ui.newPolygonButton = ui.modules.button.newPolygonButton
@@ -18,6 +19,10 @@ ui.newAngleButton = ui.modules.button.newAngleButton
 ui.newDial = ui.modules.dial.new
 ui.newScrollViewport = ui.modules.scroll.newViewport
 ui.newSlider = ui.modules.slider.new
+ui.newTextbox = ui.modules.textbox.new
+
 ui.newSet = ui.modules.set.new
 
 return ui
+
+--https://en.wikipedia.org/wiki/Graphical_widget
